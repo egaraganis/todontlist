0a3697a2dfdf382d6233e8c68ecb4aaaf6f21e3e
devme <developpmentt@gmail.com>
2018-11-21 17:45:56 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var todoController = require('./controllers/todoController') // require controllers
+
+var app = express();
+
+// set up template engine
+app.set('view engine','ejs');
+
+// static files , our middleware
+app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
+
+// listen to port
+var port= 3002;
+app.listen(port);
+console.log('Listening to port ' + port);
+
+// Express: Provides small and robust tooling for HTTP servers
+// app.set : Express instance to store and retrieve variables
+// app.use: Used as middleware
+
+// EJS: Simlple templating language that lets you generate HTML markup with plain JavaScript
+// Body-Parser: Body Parsing Middleware
---
+++ controllers/todoController.js
@@ -0,0 +1,39 @@
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false}); // middleware
+
+var data = []; // todo items will be stored here
+
+module.exports = function(app){
+  // set up simple routing , handle http requests
+  app.get('/todo',function(req,res) {
+      res.render('todo',{todos:data});
+  });
+
+  app.post('/todo',urlencodedParser,function(req,res){
+    var todoitem = Object.values(req.body);
+    data.push(todoitem[0]);
+    var i;
+    for(i=0;i<data.length;i++){
+      console.log(data[i]);
+    }
+    res.redirect(req.originalUrl);
+  });
+
+  app.delete('/todo',urlencodedParser,function(req,res){
+    var item = Object.values(req.body);
+    var found = data.indexOf(item[0]);
+    while (found !== -1) {
+      data.splice(found, 1);
+      found = data.indexOf(item);
+    }
+    try{
+      console.log('trying redirect');
+      res.redirect(req.originalUrl);
+    }
+    catch(err){
+      console.log('cannot redirect');
+    }
+  });
+}
